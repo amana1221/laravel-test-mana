533218e0e2524e74aa9b44e81acecd9235d205cf
amana1221 <62594393+amana1221@users.noreply.github.com>
2020-08-10 14:54:38 +0900

    Add files via upload

---
+++ resources/views/test.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <p>Hello World!</p>
+Jun/04/2018<p />
+
+ <p>{{$denwa}}</p>
+ 
+ <p><?php echo $denwa; ?></p>
+ 
+</body>
+</html>
