35440217ad8f301b31bf4ed6e5af37ffb5ed8a83
amana1221 <62594393+amana1221@users.noreply.github.com>
2020-08-10 14:56:14 +0900

    Add files via upload

---
+++ storage/framework/views/8085ca170c3a7da924bf7c37a6d73847ed4250cb.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <p>Hello World!</p>
+Jun/04/2018<p />
+
+ <p><?php echo e($denwa); ?></p>
+ 
+ <p><?php echo $denwa; ?></p>
+ 
+</body>
+</html><?php /**PATH C:\Users\user\Desktop\hellolaravel\resources\views/test.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/883be63ae802b5c33d4d62fe48c14fab1a43b4af.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <p>Hello World!</p>
+Jun/04/2018<p />
+</body>
+</html><?php /**PATH C:\Users\user\Desktop\hellolaravel\resources\views/hello.blade.php ENDPATH**/ ?>
